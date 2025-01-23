96c6121c642aa9949605565daf0e275db6827b0a
Tom Schneider <Tom.Schneider@burkert.com>
2025-01-23 14:16:41 +0100

    fix cmake build of tlib on windows

--- build.sh
+++ build.sh
@@ -414,7 +414,7 @@ do
     if [[ "$TLIB_EXPORT_COMPILE_COMMANDS" = true ]]; then
         CMAKE_CONF_FLAGS+=" -DCMAKE_EXPORT_COMPILE_COMMANDS=1"
     fi
-    cmake "$CMAKE_GEN" $CMAKE_COMMON $CMAKE_CONF_FLAGS -DHOST_ARCH=$HOST_ARCH $CORES_PATH
+    cmake "$CMAKE_GEN" $CMAKE_COMMON $CMAKE_CONF_FLAGS -DHOST_ARCH=$HOST_ARCH "$(get_path "$CORES_PATH")"
     cmake --build . -j$(nproc)
     CORE_BIN_DIR=$CORES_BIN_PATH/lib
     mkdir -p $CORE_BIN_DIR
